f0a352be0ba4cde8957339972ebbdf995154aeb0
athar <atharvathorat21@gmail.com>
2024-09-20 00:02:21 +0530

    Change dateOfBirth type to Local Date

--- src/domain/Guest.java
+++ src/domain/Guest.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,7 +16,7 @@ import java.util.List;
 public class Guest {
 
     private String name;
-    private String dateOfBirth;
+    private LocalDate dateOfBirth;
     private String country;
     private String language;
     private String hobby;
@@ -24,35 +25,35 @@ public class Guest {
         List<Guest> guestList = new ArrayList<>();
         guestList.add(Guest.builder()
                 .name("Madhav")
-                .dateOfBirth("1998/11/25")
+                .dateOfBirth(LocalDate.of(1999,10,22))
                 .country("USA")
                 .language("English").hobby("Eat")
                 .build());
 
         guestList.add(Guest.builder()
                 .name("Gopal")
-                .dateOfBirth("1998/11/25")
+                .dateOfBirth(LocalDate.of(1947,8,15))
                 .country("China")
                 .language("Chinese").hobby("Read")
                 .build());
 
         guestList.add(Guest.builder()
                 .name("Lakshman")
-                .dateOfBirth("1998/11/25")
+                .dateOfBirth(LocalDate.of(1952,9,1))
                 .country("Spain")
                 .language("Spanish").hobby("Eat")
                 .build());
 
         guestList.add(Guest.builder()
                 .name("Lucky")
-                .dateOfBirth("1998/11/25")
+                .dateOfBirth(LocalDate.of(1990,2,5))
                 .country("France")
                 .language("French").hobby("Sports")
                 .build());
 
         guestList.add(Guest.builder()
                 .name("Jack")
-                .dateOfBirth("1998/11/25")
+                .dateOfBirth(LocalDate.of(1966,5,29))
                 .country("Germany")
                 .language("German").hobby("Eat")
                 .build());
